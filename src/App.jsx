2d949d5ab4fa57e15dbc8b0bed7031abe7c26a02
Md Maruf Hossain <mickeymaruf@gmail.com>
2023-05-12 14:49:52 +0600

    transfered site in different app

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Invite from './Pages/Invite/Invite'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <Invite />
+  )
+}
+
+export default App
---
+++ src/Pages/Invite/Components/GetInvitedModal.jsx
@@ -0,0 +1,9 @@
+import logo from '../../../assets/logos/adbeta.jpeg';
+
+const GetInvitedModal = ({ setShowRegisterModal }) => {
+    return (
+        <></>
+    );
+};
+
+export default GetInvitedModal;
